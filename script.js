4d044aaf209161fe81e5e0987c9e4ad55c168071
weirdnomad <petitpasm@icloud.com>
2024-06-22 11:40:53 +0200

    add button to change grid size on demand

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-const container = document.querySelector(".container");
-const sizeButton = document.querySelector("button")
-
-
+const body = document.querySelector("body");
+const sizeButton = document.querySelector("button");
 addBox(16);
-
 function addBox(number) {
+  const container = document.querySelector(".container");
   if (number > 100) number = 100;
   for (j = 0; j < number; j++) {
     const row = document.createElement("div");
@@ -21,3 +19,18 @@ function addBox(number) {
     }
   }
 }
+
+  sizeButton.addEventListener("click", function () {
+    clear();
+    const userSize = prompt("Change the size of the grid (1 to 100):");
+    addBox(userSize);
+  });
+
+  function clear() {
+    const container = document.querySelector(".container");
+    container.remove();
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("container");
+    body.appendChild(newDiv);
+  }
+
