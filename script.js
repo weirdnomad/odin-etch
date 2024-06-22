f01fcd6628b2145ca75894c4daaf6cd0aa8e7383
weirdnomad <petitpasm@icloud.com>
2024-06-22 11:10:23 +0200

    functional drawing max size 100*100

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const container = document.querySelector(".container");
+const sizeButton = document.querySelector("button")
+
+
+addBox(16);
 
 function addBox(number) {
+  if (number > 100) number = 100;
   for (j = 0; j < number; j++) {
     const row = document.createElement("div");
     row.classList.add("row");
@@ -8,8 +13,11 @@ function addBox(number) {
     for (i = 0; i < number; i++) {
       const pixel = document.createElement("div");
       pixel.classList.add("box");
+      pixel.classList.add("draw");
       row.appendChild(pixel);
+      pixel.addEventListener("mouseover", () => {
+        pixel.style.backgroundColor = "black";
+      });
     }
   }
 }
-addBox(10);
