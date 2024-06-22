3b06e181bb7cb8d595effcffacb8807cdb06daaf
weirdnomad <petitpasm@icloud.com>
2024-06-22 12:04:05 +0200

    add some visuals add opacity variation

--- script.js
+++ script.js
@@ -14,23 +14,32 @@ function addBox(number) {
       pixel.classList.add("draw");
       row.appendChild(pixel);
       pixel.addEventListener("mouseover", () => {
-        pixel.style.backgroundColor = "black";
+        // pixel.style.backgroundColor = "black";
+        incOpacity();
+        function incOpacity() {
+          const currentOpacity = parseFloat(
+            window.getComputedStyle(pixel).getPropertyValue("--pixelOpacity")
+          );
+          const opacity = Math.min(currentOpacity + 0.1, 1);
+        
+          pixel.style.setProperty("--pixelOpacity", opacity);
+        }
       });
     }
   }
 }
 
-  sizeButton.addEventListener("click", function () {
-    clear();
-    const userSize = prompt("Change the size of the grid (1 to 100):");
-    addBox(userSize);
-  });
+sizeButton.addEventListener("click", function () {
+  clear();
+  const userSize = prompt("Change the size of the grid (1 to 100):");
+  addBox(userSize);
+});
 
-  function clear() {
-    const container = document.querySelector(".container");
-    container.remove();
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("container");
-    body.appendChild(newDiv);
-  }
+function clear() {
+  const container = document.querySelector(".container");
+  container.remove();
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("container");
+  body.appendChild(newDiv);
+}
 
